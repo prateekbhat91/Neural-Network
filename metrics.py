b324a59452e65501b61ac1090cd78dcff8708a23
prateekbhat91 <prateekbhat91@.com>
2016-11-06 20:28:56 -0500

    added comments

--- metrics.py
+++ metrics.py
@@ -1,9 +1,13 @@
+'''
+This file contains implementation of common metric functions for
+classification and regression.
+'''
+
 import utils
 import numpy as np
 
-
 'Classification'
-def accuracy_score(true,pred):
+def accuracy_score(true, pred):
     '''
     :param true: vector containing all the true classes
     :param pred: vector containing all the predicted classes
@@ -12,14 +16,44 @@ def accuracy_score(true,pred):
     true = utils.convert_to_1D(true)
     pred = utils.convert_to_1D(pred)
     if true.shape == pred.shape:
-        return np.count_nonzero(np.subtract(true,pred))/true.shape[0]
+        return np.count_nonzero(np.subtract(true, pred)) / true.shape[0]
     else:
         raise ValueError("true and pred dimensions do not match.")
 
 
+def confusion_matrix(true, pred):
+    '''
+
+    :param true: numpy array containing all the true classes.
+    :param pred: numpy array containing all the predicted classes.
+    :return    : confusion matrix
+    '''
+    true = utils.convert_to_1D(true)
+    pred = utils.convert_to_1D(pred)
+
+    if true.shape == pred.shape:
+        # get the number of classes
+        numclass = len(np.unique(true))
+
+        # encode the classes with integers ranging from 0 to numclass-1.
+        labelEncoder = utils.label_encoder()
+        labelEncoder.fit(true)
+        true = labelEncoder.transform(true)
+        pred = labelEncoder.transform(pred)
+
+        # create confusion matrix.
+        # Rows indicate the true class and column indicate the predicted class.
+        cm = np.array([np.zeros(numclass) for _ in range(numclass)])
+        for t, p in zip(true, pred):
+            cm[t][p] += 1
+        return cm
+
+    else:
+        raise ValueError("true and pred dimensions do not match.")
+
 
 'Regression'
-def mean_absolute_error(true,pred):
+def mean_absolute_error(true, pred):
     '''
     :param true: vector containing all the true classes
     :param pred: vector containing all the predicted classes
@@ -28,11 +62,12 @@ def mean_absolute_error(true,pred):
     true = utils.convert_to_1D(true)
     pred = utils.convert_to_1D(pred)
     if true.shape == pred.shape:
-        return np.sum(np.fabs(np.subtract(true,pred)))/true.shape[0]
+        return np.sum(np.fabs(np.subtract(true, pred))) / true.shape[0]
     else:
         raise ValueError("true and pred dimensions do not match.")
 
-def mean_squared_error(true,pred):
+
+def mean_squared_error(true, pred):
     '''
     :param true: vector containing all the true classes
     :param pred: vector containing all the predicted classes
@@ -46,7 +81,7 @@ def mean_squared_error(true,pred):
         raise ValueError("true and pred dimensions do not match.")
 
 
-def r2_score(true,pred):
+def r2_score(true, pred):
     '''
    :param true: vector containing all the true classes
    :param pred: vector containing all the predicted classes
@@ -55,42 +90,8 @@ def r2_score(true,pred):
     true = utils.convert_to_1D(true)
     pred = utils.convert_to_1D(pred)
     if true.shape == pred.shape:
-        numerator   = np.sum(np.square(np.subtract(true,pred)))
-        denominator = np.sum(np.square(np.subtract(true,np.mean(true))))
-        return 1-(numerator/denominator)
+        numerator = np.sum(np.square(np.subtract(true, pred)))
+        denominator = np.sum(np.square(np.subtract(true, np.mean(true))))
+        return 1 - (numerator / denominator)
     else:
         raise ValueError("true and pred dimensions do not match.")
-
-
-def confusion_matrix(true,pred):
-    '''
-
-    :param true: vector containing all the true classes
-    :param pred: vector containing all the predicted classes
-    :return    : confusion matrix
-    '''
-    true = utils.convert_to_1D(true)
-    pred = utils.convert_to_1D(pred)
-
-    if true.shape == pred.shape:
-        numclass = len(np.unique(true))
-        labelEncoder = utils.label_encoder()
-        labelEncoder.fit(true)
-        true = labelEncoder.transform(true)
-        pred = labelEncoder.transform(pred)
-
-        cm = [[0 for _ in range(numclass)] for _ in range(numclass)]
-        cm = np.array(cm)
-        for p, e in zip(pred,true):
-            cm[e][p] += 1
-        return cm
-
-    else:
-        raise ValueError("true and pred dimensions do not match.")
-
-if __name__ == '__main__':
-    b = [1, 2, 4, 1, 2, 5, 4, 5, 2, 3, 4, 2]
-    b = np.array(b)
-    pred = [2,1,4,1,2,4,5,2,3,4,2,5]
-    cm = confusion_matrix(b,pred)
-    print(cm)

27c70f3d3728139dea5135736888db1848cf6b93
prateekbhat91 <prateekbhat91@.com>
2016-11-06 19:24:35 -0500

    function for confusion matrix

--- metrics.py
+++ metrics.py
@@ -50,13 +50,47 @@ def r2_score(true,pred):
     '''
    :param true: vector containing all the true classes
    :param pred: vector containing all the predicted classes
-   :return: R-squared(coefficient of determination)
+   :return    : R-squared(coefficient of determination)
    '''
     true = utils.convert_to_1D(true)
     pred = utils.convert_to_1D(pred)
     if true.shape == pred.shape:
-        numerator = np.sum(np.square(np.subtract(true,pred)))
+        numerator   = np.sum(np.square(np.subtract(true,pred)))
         denominator = np.sum(np.square(np.subtract(true,np.mean(true))))
         return 1-(numerator/denominator)
     else:
         raise ValueError("true and pred dimensions do not match.")
+
+
+def confusion_matrix(true,pred):
+    '''
+
+    :param true: vector containing all the true classes
+    :param pred: vector containing all the predicted classes
+    :return    : confusion matrix
+    '''
+    true = utils.convert_to_1D(true)
+    pred = utils.convert_to_1D(pred)
+
+    if true.shape == pred.shape:
+        numclass = len(np.unique(true))
+        labelEncoder = utils.label_encoder()
+        labelEncoder.fit(true)
+        true = labelEncoder.transform(true)
+        pred = labelEncoder.transform(pred)
+
+        cm = [[0 for _ in range(numclass)] for _ in range(numclass)]
+        cm = np.array(cm)
+        for p, e in zip(pred,true):
+            cm[e][p] += 1
+        return cm
+
+    else:
+        raise ValueError("true and pred dimensions do not match.")
+
+if __name__ == '__main__':
+    b = [1, 2, 4, 1, 2, 5, 4, 5, 2, 3, 4, 2]
+    b = np.array(b)
+    pred = [2,1,4,1,2,4,5,2,3,4,2,5]
+    cm = confusion_matrix(b,pred)
+    print(cm)

47bdfe135a7b6e4c6f972029830762622e1d0fa9
prateekbhat91 <prateekbhat91@.com>
2016-11-17 13:30:57 -0500

    new files

---
+++ neuralnetwork/Layers/base_layer.py
@@ -0,0 +1,47 @@
+from __future__ import division
+from abc import abstractmethod, ABCMeta
+import numpy as np
+from neuralnetwork.activation_functions import *
+np.random.seed(2488)
+
+
+class BaseLayer():
+    '''
+    Create Layer object for Neural Network and store all the informations related to it.
+    '''
+    __metaclass__ = ABCMeta
+
+    def __init__(self,input_dim,output_dim,activation_function):
+        '''
+        :param input_dim: Input Dimension for this Layer
+        :param output_dim: Output Dimension for this Layer
+        :param transformFunction: Non-Linear transform Function to be applied on the output result
+        '''
+
+        assert (input_dim > 0 and isinstance(input_dim, int)  and output_dim >0 and isinstance(output_dim, int)), \
+            'Input and Output dimensions should be integers greater than zero, ' \
+            'instead received input_dim = {0} and output_dim = {1}'.format(input_dim, output_dim)
+
+        self.inputDim = input_dim
+        self.outputDim = output_dim
+        self.activation_function = activation_function
+        self.weights = self.__set_weights(self.inputDim, self.outputDim)
+        self.bias = np.zeros((1,output_dim))
+
+
+    def __set_weights(self,input_dim, output_dim):
+        return np.random.normal(0,1,output_dim*input_dim).reshape(input_dim,output_dim)
+
+    @abstractmethod
+    def derivative(self,x):
+        pass
+
+    def __str__(self):
+        '''
+        Overrides print function of python.
+        :return: layer details.
+        '''
+        return '{:>10s}{:>18d}\n'.format('Input dimension', self.inputDim) + \
+               '{:>10s}{:>17d}\n'.format('Output dimension', self.outputDim) + \
+               '{:>s}{:>18s}\n'.format('Activation Function', self.activation_function.__name__)+ \
+               '{:>10s}{:>23s}\n'.format('Weights Shape', self.weights.shape)
---
+++ neuralnetwork/Layers/layers.py
@@ -0,0 +1,69 @@
+from base_layer import BaseLayer
+from neuralnetwork.activation_functions import *
+
+class Sigmoid_Layer(BaseLayer):
+
+    def __init__(self,input_dim,output_dim):
+        '''
+        :param input_dim : input dimension of layer
+        :param output_dim: output dimension of layer
+        '''
+        BaseLayer.__init__(self,input_dim,output_dim, activation_function = sigmoid)
+
+    def derivative(self, x):
+        '''
+        :param x: sigmoid activated input.
+        :return: derivative of sigmoid
+        '''
+        return np.multiply(x, 1 - x)
+
+
+class Tanh_Layer(BaseLayer):
+
+    def __init__(self,input_dim,output_dim):
+        '''
+        :param input_dim : input dimension of layer
+        :param output_dim: output dimension of layer
+        '''
+        BaseLayer.__init__(self,input_dim,output_dim, activation_function = tanh)
+
+    def derivative(self, x):
+        '''
+        :param x: tanh activated input.
+        :return: derivative of sigmoid
+        '''
+        return 1 - np.power(x, 2)
+
+class ReLU_Layer(BaseLayer):
+
+    def __init__(self,input_dim,output_dim):
+        '''
+        :param input_dim: input dimension of layer
+        :param output_dim: output dimension of layer
+        '''
+        BaseLayer.__init__(self, input_dim, output_dim, activation_function=ReLU)
+
+    def derivative(self, x):
+        '''
+        :param x:
+        :return: derivate of tanh function
+        '''
+        return (x > 0)
+
+
+class LeakyReLU_Layer(BaseLayer):
+
+    def __init__(self,input_dim,output_dim):
+        '''
+        :param input_dim: input dimension of layer
+        :param output_dim: output dimension of layer
+        '''
+        BaseLayer.__init__(self, input_dim, output_dim, activation_function=Leaky_ReLU)
+
+
+    def derivative(self, x):
+        '''
+        :param x: Leaky ReLU activated input.
+        :return: derivate of Leaky ReLU with leakage of 0.01.
+        '''
+        return np.clip(x > 0, 0.01, 1.0)
